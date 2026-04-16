c6761dda0917700a0376cc25c10bca3905cc0398
SinRan <sempakordis059@gmail.com>
2026-04-16 11:31:52 +0700

    fix command URL

--- app/Http/Controllers/Api/CommandController.php
+++ app/Http/Controllers/Api/CommandController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Command;
+use App\Models\Commands;
 
 class CommandController extends Controller
 {
